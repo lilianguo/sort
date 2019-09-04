b017ba19e557f35aa435ac72ab65eb04ebfe2e17
linqig <linqig@amazon.com>
2019-09-04 10:23:37 -0700

    Create MeetingRooms.java

---
+++ MeetingRooms.java
@@ -0,0 +1,14 @@
+public class MeetingRoom {
+    public boolean canAttendMeetings(int[][] intervals) {
+        if (intervals == null || intervals.length == 0) {
+            return true;
+        }
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i - 1][1] > intervals[i][0]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
