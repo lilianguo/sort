f3f5c1c189766f3346372e2918ced1dc42cadcc6
linqig <linqig@amazon.com>
2019-09-05 17:45:12 -0700

    Update MeetingRooms.java

--- MeetingRooms.java
+++ MeetingRooms.java
@@ -11,4 +11,23 @@ public class MeetingRoom {
         }
         return true;
     }
+
+    public boolean canAttendMeetingsII(Interval[] intervals) {
+        if (intervlas == null || intervals.length == 0) {
+            return true;
+        }
+        Arrays.sort(intervals, new IntervalComparator());
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i].start < intervals[i - 1].end) {
+                return false;
+            }
+            return true;
+        }
+    }
+
+    private class IntervalComparator {
+        public int compare(Interval a, Interval b) {
+            return a.start - b.start;
+        }
+    }
 }
